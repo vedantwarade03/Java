e2fed9f01f2f17348c1e5f34a1d49657fcfed6c7
vedantwarade03 <vedantwarade03@gmail.com>
2026-04-09 08:21:51 +0530

    Create Restaurant class for food preparation and serving

---
+++ Assignment_41.java
@@ -0,0 +1,5 @@
+Producer Consumer System
+
+Create a class Restaurant. Chef prepares food and waiter serves
+it using wait() and notify().
+Output: &#39;Chef prepared food&#39; and &#39;Waiter served food&#39;
